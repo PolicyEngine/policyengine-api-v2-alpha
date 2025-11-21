9c58b9cf895092cdde4cb12df11484f59db4ce0f
Nikhil Woodruff <nikhil.woodruff@outlook.com>
2025-11-21 12:40:53 +0000

    Update

--- terraform/main.tf
+++ terraform/main.tf
@@ -245,6 +245,13 @@ resource "aws_security_group" "ecs" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
   tags = {
     Name = "${var.project_name}-ecs-sg"
   }
