1f88c233565e5bf4a30cd2c8def386f136feb482
SakshiKekre <sakshi.kekre@gmail.com>
2026-03-11 17:57:34 +0530

    fix: Sort imports in migration file

--- alembic/versions/20260310_67608331ee8a_add_parameter_nodes_table.py
+++ alembic/versions/20260310_67608331ee8a_add_parameter_nodes_table.py
@@ -7,10 +7,10 @@ Create Date: 2026-03-10 18:29:54.555074
 """
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
 import sqlmodel.sql.sqltypes
 
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = '67608331ee8a'
